9a4f1099b384471f67d321dfcf25a5a09f50c86c
yumaster <yumaster@yeah.net>
2019-11-11 11:37:48 +0800

    update-单例模式

--- 单例模式/Program.cs
+++ 单例模式/Program.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
+/*
+ * 单例模式
+ * 意图：保证一个类仅有一个实例，并提供一个访问它的全局访问点
+ * 主要解决：一个全局使用的类频繁地创建与销毁
+ * 何时使用：当你想控制实例数目，节省系统资源的时候
+ * 如何解决：判断系统中是否已经有这个单例，如果有则返回，如果没有则创建
+ * 关键代码：构造函数是私有的
+ * 优点：在内存里只有一个实例，减少了内存的开销，尤其是频繁的创建和销毁实例；避免对资源的多重占用（比如写文件操作）
+ * 缺点：没有接口，不能继承，与单一职责原则冲突，一个类应该只关心内部逻辑，而不关心外面怎么样来实例化。
+ */
 namespace 单例模式
 {
     class Program
@@ -68,6 +78,7 @@ namespace 单例模式
     /// </summary>
     public class Singleton
     {
+        //让构造函数为 private，这样该类就不会被实例化
         private Singleton()
         {
             Console.WriteLine("{0}被创建了，线程ID{1}！", this.GetType().Name, Thread.CurrentThread.ManagedThreadId);
@@ -100,6 +111,7 @@ namespace 单例模式
     public class SingletonSecond
     {
         public static SingletonSecond singleton = null;
+        //让构造函数为 private，这样该类就不会被实例化
         private SingletonSecond()
         {
             Console.WriteLine("{0}被创建了,线程ID{1}！", this.GetType().Name, Thread.CurrentThread.ManagedThreadId);
@@ -125,6 +137,7 @@ namespace 单例模式
     /// </summary>
     public class SingletonThird
     {
+        //让构造函数为 private，这样该类就不会被实例化
         private SingletonThird()
         {
             Console.WriteLine("{0}被创建了,线程ID{1}！", this.GetType().Name, Thread.CurrentThread.ManagedThreadId);
